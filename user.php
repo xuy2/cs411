00f4db92e07b78c5a3ee32ce0475971834162aac
Qing Ye <qing@wirelessprv-10-192-50-207.near.illinois.edu>
2017-03-11 15:11:59 -0600

    finished the user profile user story

--- user.php
+++ user.php
@@ -44,39 +44,49 @@
 
 <html>
 <head>
-<meta charset= 'utf-8'>
-<title> Welcome to schoolHunter</title>
+    <meta charset= 'utf-8'>
+    <title> Welcome to schoolHunter</title>
 </head>
 <body>
 
-	<form action="user_form.php" method='POST' enctype='multipart/form-data'>
-    <div class="container" align ='center'>
-    				<input type="hidden" name="Register" value = "true">
-      				<label><b>Email: </b></label>
-      				<input type="email" value="<?=$email?>" name="Email" readonly="readonly">
-      				<br/>
-      				<label><b>Password: </b></label>
-      				<input type="password" value="<?=$password?>" name="Password">
-        			<br/>
-        			<label><b>Gender: </b></label>
-        			<input type="radio" name="Gender" value="M" <?=is_male($gender)?>/> Male
-				      <input type="radio" name="Gender" value="F" <?=is_female($gender)?>/> Female
-        			<br/>
-        			<label><b>GRE Verbal: </b></label>
-      				<input type="text" value=<?=$gre_v?> name="GRE_V">
-      				<br/>
-      				<label><b>GRE Quantitative: </b></label>
-      				<input type="text" value=<?=$gre_q?> name="GRE_Q">
-      				<br/>
-      				<label><b>GRE Analytical Writing: </b></label>
-      				<input type="text" value=<?=$gre_aw?> name="GRE_AW">
-        			<br/>
-        			<label><b>GPA: </b></label>
-      				<input type="text" value=<?=$gpa?> name="GPA">
-        			<br/>
-        			<label><b>TOEFL: </b></label>
-      				<input type="text" value=<?=$toefl?> name="TOEFL">
-      				<br/>
-      				<button type="submit"> Update</button>
-    </div>
-	</form>
+
+
+<div class="container" align ='center'>
+    <button onclick = "window.location = 'user_edit.php'">Edit</button>
+    <table>
+        <tr>
+            <td>Email</td>
+            <td><?=$email?></td>
+        </tr>
+
+        <tr>
+            <td>Gender</td>
+            <td><?=$gender?></td>
+        </tr>
+
+        <tr>
+            <td>GRE Verval</td>
+            <td><?=$gre_v?></td>
+        </tr>
+
+        <tr>
+            <td>GRE Quantitative</td>
+            <td><?=$gre_q?></td>
+        </tr>
+
+        <tr>
+            <td>GRE Analytical Writing</td>
+            <td><?=$gre_aw?></td>
+        </tr>
+
+        <tr>
+            <td>GPA</td>
+            <td><?=$gpa?></td>
+        </tr>
+
+        <tr>
+            <td>TOEFL</td>
+            <td><?=$toefl?></td>
+        </tr>
+    </table>
+</div>
